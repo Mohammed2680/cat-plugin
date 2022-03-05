86b4dae9874ddcf791d114485bce9eff42260f39
Mohammed2680 <76847663+Mohammed2680@users.noreply.github.com>
2022-03-05 04:11:24 +0300

    add close

--- whatsapp.py
+++ whatsapp.py
@@ -8,6 +8,8 @@ AGWhatsapp or many whatsapp : https://www.3ssem.com/?m=1
 
 NAWhatsapp : https://en.naljaede.com/?m=1
 
+MTWhatsapp : https://mtwhatsapp2021.blogspot.com
+
 ArWhatsapp or more : https://asem123456x.blogspot.com/?m=1
 
 AKR whatsapp : https://akrappstore.blogspot.com/2021/07/akr-whatsapp-v630_22.html?m=1
